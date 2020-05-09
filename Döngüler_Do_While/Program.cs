9ce7b115093f560fde5a62c2955b199730b28e47
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-09 23:14:27 +0300

    Initial commit

---
+++ Döngüler_Do_While/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Döngüler_Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool durum = false;
+            do
+            {
+                Console.WriteLine("Hello World.");
+            }
+            while (durum); // durum değişkeni false olduğu için 1 kere çalışır.
+            int i = 10;
+            do
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            while (i > 0); // Koşul 0 dan büyük olduğu sürece birer birer azaltarak ekrana i değişkenini yazmaya devam et.
+            Console.ReadKey();
+        }
+    }
+}
+/* Diğer döngülerde önce koşula bakılır. Daha sonra işlem yapılır.
+   Do while döngüsünde ise önce işlem yapılır sonra koşula bakılır.
+   Bu nedenle koşul sağlanıyor olsa bile döngü bir kere çalışır.
+   do
+   {
+       //<İşlemler>
+   }
+   while (<koşul>);
+*/
---
+++ README.md
@@ -0,0 +1,2 @@
+# Do-While-Loop
+ Do-While döngüsü. / Do-While Loop.
